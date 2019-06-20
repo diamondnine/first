34a201a700cfc5851956b9ac5686ea114a43f2e1
diamondnine <1582774238@qq.com>
2019-06-20 16:37:21 +0800

    qt学习19620

---
+++ Desktop/mqt19620/mydialog1/mydialog1.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2019-06-20T08:07:01
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = mydialog1
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mywidget.cpp \
+    mydialog.cpp
+
+HEADERS  += mywidget.h \
+    mydialog.h
+
+FORMS    += mywidget.ui \
+    mydialog.ui
---
+++ Desktop/mqt19620/mywidget1/main.cpp
@@ -0,0 +1,20 @@
+#include<QtWidgets>
+
+int main(int argc,char* argv[])
+{
+    QApplication a(argc,argv);
+    QWidget* widget = new QWidget();//QWidget* widget = new QWidget();
+    widget->setWindowTitle(QObject::tr("我是widget"));
+    QLabel* label = new QLabel();//QLabel* label = new QLabel();
+    label->setWindowTitle(QObject::tr("我是label"));
+    label->resize(180,20);
+    QLabel* label2 = new QLabel(widget);
+    label2->setText("label2：我不是独立窗口，知识widget的子部件！");
+    label2->resize(250,20);
+    label->show();
+    widget->show();
+    int ret = a.exec();
+    delete label;
+    delete widget;
+    return ret;
+}
